ab4bdb28ab0d259051ac7d51a423ba91074c10f1
vinicioslop <vinicioslop7@hotmail.com>
2021-09-29 16:31:56 -0300

    Implementa estrutura básica do projeto

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace sistema_escolar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Bem vindo ao Sistema Escolar Simplificado!");
+        }
+    }
+}
